a21aea4c9c516e147af98496599a3f1ec1cd51a6
Jordan Leggett <user1pc@hotmail.com>
2021-02-14 21:21:42 -0800

    removed extra blank lines

--- UnitTests/test_square_right_of_line_traverser.cpp
+++ UnitTests/test_square_right_of_line_traverser.cpp
@@ -161,30 +161,6 @@ TEST(SquareRightOfLineTraverser, CompleteTraversal)
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 void test_square_right_of_line_traverser_fill_random_correct(int32_t x1, int32_t y1, int32_t x2, int32_t y2,
     int32_t square_x, int32_t square_y, int32_t square_width_log2,
     uint8_t* pixels, int32_t width, int32_t height,
